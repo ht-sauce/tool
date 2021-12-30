081b0d7cae29cfcc1ee7e41757776173add883ba
haitian.dai <haitian.dai@ctg.cn>
2021-12-30 15:23:39 +0800

    正则表达式更新

--- regxp.js
+++ regxp.js
@@ -20,7 +20,7 @@ export const numberOrChar = (str) =>
   /^[0-9  \!\@\#\$\^\&\*\-\_\+\=\:\\\'\;\,\.\/\?\(\)\{\}\[\]\<\>]*$/g.test(str)
 // 只能输入指定小数位数的数字
 export const decimalLen = (str, len = 2) => {
-  return new RegExp(`^([1-9][0-9]*)+(\.[0-9]{1,` + len + `})?$`, 'g').test(str)
+  return new RegExp(`^([1-9][0-9]*)+(\.[0-9]{1,` + len + `})?$`, 'g').test(String(str))
 }
 // 只能输入数字和小数点，不限制小数点位置
 export const numOrPoint = (str) => /^[0-9]{0}([0-9]|[.])*$/g.test(str)
@@ -28,17 +28,19 @@ export const numOrPoint = (str) => /^[0-9]{0}([0-9]|[.])*$/g.test(str)
 export const largeZeroInt = (str) => /^\+?[1-9]\d*$/g.test(str)
 // 只能输入1-99
 export const onnToInetyNine = (str) => /^[1-9][0-9]?$/g.test(str)
+// 输入1-100
+export const oneToOneHundred = (str) => /^[1-9][0-9]?$|100/g.test(str)
 // 只能输入0.1到100的数字
 export const zeroPointOneToOneHundred = (str) => /0\.[1-9]|^[1-9]?$|^[1-9][0-9]?$|100/g.test(str)
-// 只能输入字母数字汉字和+-,()
-export const zysStr = (str) => /^[A-Za-z0-9\u4e00-\u9fa5]+$|[+-,()]/g.test(str)
+// 只能输入字母数字汉字和+-,()，可以输入空格
+export const zysStr = (str) => /^[\u4E00-\u9FA5A-Za-z0-9+\-\,\(\) ]+$/g.test(str)
 // 指定小数点前面最大位数和小数点后面位数的数字
 export const sePointNum = (str, len1 = 5, len2 = 2) => {
-  return new RegExp('^[0-9]{0,' + len1 + '}(\\.\\d{1,' + len2 + '})?$', 'g').test(str)
+  return new RegExp('^[0-9]{0,' + len1 + '}(\\.\\d{1,' + len2 + '})?$', 'g').test(String(str))
 }
 // 输入1-17岁
 export const oneToEighteen = (str) => /^(1[0-7]|[1-9])$/.test(str)
 // 输入n位数字1-9
 export const numberLen = (str, len = 1) => {
-  return RegExp('^[1-9]{1,' + len + '}$', 'g').test(str)
+  return RegExp('^[1-9]{1,' + len + '}$', 'g').test(String(str))
 }
