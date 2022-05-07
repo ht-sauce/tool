dab0857fc068cb8be5683c121ea4c0397e39fd58
haitian.dai <haitian.dai@ctg.cn>
2022-05-07 14:30:48 +0800

    正则表达式更新一下

--- regxp.js
+++ regxp.js
@@ -12,12 +12,12 @@ export function isExternal(path) {
 export const zeroOrNumber = (value) => /^([0-9]*)$/.test(value)
 // 验证手机号
 export const checkPhone = (val) =>
-  /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/.test(val)
+    /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/.test(val)
 // 验证汉字
 export const chineseChar = (str) => /[\u4E00-\u9FA5]/g.test(str)
 // 匹配数字和特殊字符
 export const numberOrChar = (str) =>
-  /^[0-9  \!\@\#\$\^\&\*\-\_\+\=\:\\\'\;\,\.\/\?\(\)\{\}\[\]\<\>]*$/g.test(str)
+    /^[0-9  \!\@\#\$\^\&\*\-\_\+\=\:\\\'\;\,\.\/\?\(\)\{\}\[\]\<\>]*$/g.test(str)
 // 只能输入指定小数位数的数字
 export const decimalLen = (str, len = 2) => {
   return new RegExp(`^([1-9][0-9]*)+(\.[0-9]{1,` + len + `})?$`, 'g').test(String(str))
@@ -28,6 +28,10 @@ export const numOrPoint = (str) => /^[0-9]{0}([0-9]|[.])*$/g.test(str)
 export const largeZeroInt = (str) => /^\+?[1-9]\d*$/g.test(str)
 // 只能输入1-99
 export const onnToInetyNine = (str) => /^[1-9][0-9]?$/g.test(str)
+// 只能输入0-99
+export const zeroToInetyNine = (str) => /^[0-9][0-9]?$/g.test(str)
+// 只能输入0-60,不包含60
+export const zeroToSixty = (str) => /^[0-5][0-9]?$/g.test(str)
 // 输入1-100
 export const oneToOneHundred = (str) => /^[1-9][0-9]?$|100/g.test(str)
 // 只能输入0.1到100的数字
