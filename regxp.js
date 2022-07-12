0b355f910763ceb1a4e68c0e6c3d01bda61d49c1
dht <603928588@qq.com>
2022-07-12 15:36:22 +0800

    修复0-60的数字校验和新增一些校验

--- regxp.js
+++ regxp.js
@@ -10,6 +10,8 @@ export function isExternal(path) {
 }
 // 数字或者零
 export const zeroOrNumber = (value) => /^([0-9]*)$/.test(value)
+// 大于0的整数
+export const bigZeroNumber = (value) => /^[1-9]\d*$/.test(value)
 // 验证手机号
 export const checkPhone = (val) =>
     /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/.test(val)
@@ -31,7 +33,7 @@ export const onnToInetyNine = (str) => /^[1-9][0-9]?$/g.test(str)
 // 只能输入0-99
 export const zeroToInetyNine = (str) => /^[0-9][0-9]?$/g.test(str)
 // 只能输入0-60,不包含60
-export const zeroToSixty = (str) => /^[0-5][0-9]?$/g.test(str)
+export const zeroToSixty = (str) => /^[0-9]?$|^[0-5][0-9]?$/g.test(str)
 // 输入1-100
 export const oneToOneHundred = (str) => /^[1-9][0-9]?$|100/g.test(str)
 // 只能输入0.1到100的数字
@@ -47,4 +49,16 @@ export const oneToEighteen = (str) => /^(1[0-7]|[1-9])$/.test(str)
 // 输入n位数字1-9
 export const numberLen = (str, len = 1) => {
   return RegExp('^[1-9]{1,' + len + '}$', 'g').test(String(str))
+}
+// 只能为整数
+export const isInteger = (str) => {
+  return /^-?[1-9]\d*$/.test(Number(str))
+}
+// 是否是数字，正负整数，小数
+export const isNumber = (str) => {
+  return /^-?[0-9]{0}([0-9]|[.])*$/.test(Number(str))
+}
+// 银行卡简单正则
+export const isBankCard = (str) => {
+  return /^([1-9]{1})(\d{15}|\d{16}|\d{18})$/.test(str)
 }
